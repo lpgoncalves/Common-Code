47b898b6bc0c12c1269d343f3d07a6c7672022bf
Luis Paulo Gonçalves <lp.gooncalves@gmail.com>
2017-04-10 10:21:56 -0300

    Base64 To Image and Reverse

--- C#/Common.cs
+++ C#/Common.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Web.Configuration;
+using System.Drawing;
 
 namespace Common
 {
@@ -831,6 +832,32 @@ public static class Validator
         {
             return Enumerable.Range(0, toDate.Subtract(fromDate).Days + 1)
                              .Select(d => fromDate.AddDays(d));
+        }
+
+        public Image Base64ToImage(string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(ms, true);
+                return image;
+            }
+        }
+
+        public string ImageToBase64(Image image,System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to base 64 string
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
         }
     }
 }
